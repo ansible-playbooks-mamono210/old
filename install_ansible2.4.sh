8d1ecff91aedafcf8aa59b6b2204e40b87cd0d50
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2017-05-06 23:17:57 +0900

    pipのansibleインストールコマンドが間違っていたので修正した refs #322

--- install_ansible2.4.sh
+++ install_ansible2.4.sh
@@ -1,11 +1,14 @@
 yum -y groupinstall "Development Tools" 
 yum -y install epel-release
+
 yum -y install openssl-devel
 yum -y install python-devel
 yum -y install python-pip
-pip -y install --upgrade pip
-pip -y install --upgrade setuptools
-pip -y install git+git://github.com/ansible/ansible.git@devel
-ansible --version
+
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install git+git://github.com/ansible/ansible.git@devel
 
 yum -y install expect
+
+ansible --version

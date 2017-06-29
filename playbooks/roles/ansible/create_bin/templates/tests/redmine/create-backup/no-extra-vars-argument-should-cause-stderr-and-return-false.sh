61f9acea093f9d9a67727b9cc1409e86e9d89b17
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2017-06-30 02:34:01 +0900

    bin「redmine/create_backup」を修正した refs #348

--- playbooks/roles/ansible/create_bin/templates/tests/redmine/create-backup/no-extra-vars-argument-should-cause-stderr-and-return-false.sh
+++ playbooks/roles/ansible/create_bin/templates/tests/redmine/create-backup/no-extra-vars-argument-should-cause-stderr-and-return-false.sh
@@ -14,14 +14,6 @@ test_backup_work_dir() {
     assertEquals "RPROGRAM: option requires an argument -- --backup_work_dir" "${result}" 
 }
 
-test_redmine_dir() {
-    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --redmine_dir"
-    result="$(${command} 2>&1)"
-    
-    assertEquals 1 $?
-    assertEquals "RPROGRAM: option requires an argument -- --redmine_dir" "${result}" 
-}
-
 test_db_host() {
     command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --db_host"
     result="$(${command} 2>&1)"
@@ -46,12 +38,68 @@ test_db_password() {
     assertEquals "RPROGRAM: option requires an argument -- --db_password" "${result}" 
 }
 
+test_db_port() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --db_port"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --db_port" "${result}" 
+}
+
 test_db_user() {
     command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --db_user"
     result="$(${command} 2>&1)"
     
     assertEquals 1 $?
     assertEquals "RPROGRAM: option requires an argument -- --db_user" "${result}" 
+}
+
+test_git_archive_file() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --git_archive_file"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --git_archive_file" "${result}" 
+}
+
+test_git_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --git_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --git_dir" "${result}" 
+}
+
+test_git_parent_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --git_parent_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --git_parent_dir" "${result}" 
+}
+
+test_pg_dump_data_file() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --pg_dump_data_file"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --pg_dump_data_file" "${result}" 
+}
+
+test_redmine_media_archive_file() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --redmine_media_archive_file"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --redmine_media_archive_file" "${result}" 
+}
+
+test_redmine_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/redmine/create_backup/{{ item.key }} --redmine_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --redmine_dir" "${result}" 
 }
 
 # load shunit2

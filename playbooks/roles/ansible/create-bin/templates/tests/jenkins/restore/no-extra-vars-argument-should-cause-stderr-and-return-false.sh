828c8228437782152abf0fd3a7f301dcb44442a3
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2017-09-16 21:52:59 +0900

    playbook「jenkins_resotre」を実装した refs #359

---
+++ playbooks/roles/ansible/create-bin/templates/tests/jenkins/restore/no-extra-vars-argument-should-cause-stderr-and-return-false.sh
@@ -0,0 +1,34 @@
+test_jenkins_backup_data_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/jenkins/restore/{{ item.value.name }}.sh --jenkins_backup_data_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --jenkins_backup_data_dir" "${result}" 
+}
+
+test_jenkins_backup_data_host() {
+    command="{{ ansible_user_dir }}/ansible/bin/jenkins/restore/{{ item.value.name }}.sh --jenkins_backup_data_host"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --jenkins_backup_data_host" "${result}" 
+}
+
+test_jenkins_installed_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/jenkins/restore/{{ item.value.name }}.sh --jenkins_installed_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --jenkins_installed_dir" "${result}" 
+}
+
+test_jenkins_restore_work_dir() {
+    command="{{ ansible_user_dir }}/ansible/bin/jenkins/restore/{{ item.value.name }}.sh --jenkins_restore_work_dir"
+    result="$(${command} 2>&1)"
+    
+    assertEquals 1 $?
+    assertEquals "RPROGRAM: option requires an argument -- --jenkins_restore_work_dir" "${result}" 
+}
+
+# load shunit2
+. {{ ansible_user_dir }}/ansible/bin/shunit2/source/2.1/src/shunit2
